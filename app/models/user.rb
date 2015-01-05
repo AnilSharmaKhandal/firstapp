99b38bb3873d3eb8524354867983dab095dcbbc9
anil <anilsharma.sharma76@gmail.com>
2015-01-05 15:27:16 -0800

    gem file updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,18 @@
 class User < ActiveRecord::Base
+
+	# Adds `can_create?(resource)`, etc
+  include Authority::UserAbilities
+  
+  rolify :before_add => :before_add_method
+
+  def before_add_method(role)
+    # do something before it gets added
+  end
+
+  # To update a specific resource instance, you must either own it or be an admin
+  def updatable_by?(user)
+    resource.author == user || user.has_role?(:admin)
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,6 @@ devise_scope :user do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-
   root to: "home#index"
   
   # Example of regular route:
